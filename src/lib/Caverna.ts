cab92e6bb2677f8fa89647a50d0375b1f46fee24
Anfígeno <dolorcriticodevastador@proton.me>
2024-04-05 16:03:59 -0500

    [Refactorización] Sustituimos el try catch

--- src/lib/Caverna.ts
+++ src/lib/Caverna.ts
@@ -2,7 +2,7 @@ import pino from "pino";
 
 export default class Caverna {
   private readonly urlApi = "http://0.0.0.0:8000/api";
-  private readonly log = pino();
+  protected readonly log = pino();
 
   private readonly tokenApi: string;
 
@@ -18,15 +18,15 @@ export default class Caverna {
   public async obtenerTiques(): Promise<void> {
     const url = `${this.urlApi}/tiques`;
 
-    try {
-      const respuesta = await fetch(url, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Autorizacion: this.tokenApi,
-        },
-      });
+    const respuesta = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Autorizacion: this.tokenApi,
+      },
+    });
 
+    if (respuesta.ok) {
       const datos = await respuesta.json();
 
       this.tiques = {
@@ -34,37 +34,41 @@ export default class Caverna {
         idCategoria: datos.id_categoria,
         cantidad: datos.cantidad,
       };
-    } catch (error) {
-      this.log.error(error);
 
-      await fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Autorizacion: this.tokenApi,
-        },
-      });
-
-      this.tiques = {
-        idCanalDeRegistros: null,
-        idCategoria: null,
-        cantidad: null,
-      };
+      return;
     }
+
+    const creacion = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Autorizacion: this.tokenApi,
+      },
+    });
+
+    if (!creacion.ok) {
+      throw new Error(JSON.stringify(await creacion.json()));
+    }
+
+    this.tiques = {
+      idCanalDeRegistros: null,
+      idCategoria: null,
+      cantidad: null,
+    };
   }
 
   public async obtenerRolesDeAdministracion(): Promise<void> {
     const url = `${this.urlApi}/roles_administracion`;
 
-    try {
-      const respuesta = await fetch(url, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Autorizacion: this.tokenApi,
-        },
-      });
+    const respuesta = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Autorizacion: this.tokenApi,
+      },
+    });
 
+    if (respuesta.ok) {
       const datos = await respuesta.json();
 
       this.rolesDeAdministracion = {
@@ -74,75 +78,83 @@ export default class Caverna {
         idSoporte: datos.id_soporte,
         idInterno: datos.id_interno,
       };
-    } catch (error) {
-      this.log.error(error);
 
-      await fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Autorizacion: this.tokenApi,
-        },
-      });
-
-      this.rolesDeAdministracion = {
-        idAdministrador: null,
-        idDirector: null,
-        idModerador: null,
-        idSoporte: null,
-        idInterno: null,
-      };
+      return;
     }
+
+    const creacion = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Autorizacion: this.tokenApi,
+      },
+    });
+
+    if (!creacion.ok) {
+      throw new Error(JSON.stringify(await creacion.json()));
+    }
+
+    this.rolesDeAdministracion = {
+      idAdministrador: null,
+      idDirector: null,
+      idModerador: null,
+      idSoporte: null,
+      idInterno: null,
+    };
   }
 
   public async obtenerEmbeds(): Promise<void> {
     const url = `${this.urlApi}/embeds`;
 
-    try {
-      const respuesta = await fetch(url, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Autorizacion: this.tokenApi,
-        },
-      });
+    const respuesta = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Autorizacion: this.tokenApi,
+      },
+    });
 
+    if (respuesta.ok) {
       const datos = await respuesta.json();
 
       this.embeds = {
         color: datos.color,
         urlImaginLimitadora: datos.url_imagen_limitadora,
       };
-    } catch (error) {
-      this.log.error(error);
 
-      await fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Autorizacion: this.tokenApi,
-        },
-      });
-
-      this.embeds = {
-        color: null,
-        urlImaginLimitadora: null,
-      };
+      return;
     }
+
+    const creacion = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Autorizacion: this.tokenApi,
+      },
+    });
+
+    if (!creacion.ok) {
+      throw new Error(JSON.stringify(await creacion.json()));
+    }
+
+    this.embeds = {
+      color: null,
+      urlImaginLimitadora: null,
+    };
   }
 
   public async obtenerCanalesDeRegistros(): Promise<void> {
     const url = `${this.urlApi}/canales_registros`;
 
-    try {
-      const respuesta = await fetch(url, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Autorizacion: this.tokenApi,
-        },
-      });
+    const respuesta = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Autorizacion: this.tokenApi,
+      },
+    });
 
+    if (respuesta.ok) {
       const datos = await respuesta.json();
 
       this.canalesDeRegistros = {
@@ -152,25 +164,29 @@ export default class Caverna {
         idCanalSanciones: datos.id_canal_sanciones,
         idCanalServidor: datos.id_canal_servidor,
       };
-    } catch (error) {
-      this.log.error(error);
 
-      await fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Autorizacion: this.tokenApi,
-        },
-      });
-
-      this.canalesDeRegistros = {
-        idCanalMensajes: null,
-        idCanalVoz: null,
-        idCanalUsuarios: null,
-        idCanalSanciones: null,
-        idCanalServidor: null,
-      };
+      return;
     }
+
+    const creacion = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Autorizacion: this.tokenApi,
+      },
+    });
+
+    if (!creacion.ok) {
+      throw new Error(JSON.stringify(await creacion.json()));
+    }
+
+    this.canalesDeRegistros = {
+      idCanalMensajes: null,
+      idCanalVoz: null,
+      idCanalUsuarios: null,
+      idCanalSanciones: null,
+      idCanalServidor: null,
+    };
   }
 
   public async actualizarTiques(nuevosDatos: Tiques): Promise<void> {
